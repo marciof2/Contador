1d387d32fc8a3906d792d82883395a0461d87038
marciof2 <marcinsilva26@gmail.com>
2021-04-12 19:27:24 -0300

    M01--A06

--- lib/main.dart
+++ lib/main.dart
@@ -15,50 +15,86 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   int cont = 0;
+
+  Function add() {
+    setState(
+      () {
+        cont = cont + 1;
+      },
+    );
+  }
+
+  Function minus() {
+    setState(() {
+      cont--;
+    });
+  }
+
+  Function reset() {
+    setState(() {
+      cont = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Text(
-            'Contador',
-            style: TextStyle(
-              fontSize: 25,
-            ),
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(
+          'Contador',
+          style: TextStyle(
+            fontSize: 25,
           ),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              alignment: Alignment.center,
-              child: Text(
-                '$cont',
-                style: TextStyle(fontSize: 40, color: Colors.black),
-              ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            child: Text(
+              '$cont',
+              style: TextStyle(fontSize: 40, color: Colors.black),
             ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        height: 100,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                spreadRadius: 1.0,
+                color: Colors.grey.withOpacity(.3),
+                blurRadius: 3.0),
           ],
         ),
-        floatingActionButton: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             FloatingActionButton(
               onPressed: () {
-                setState(() {
-                  cont = cont - 1;
-                });
+                minus();
               },
-              child: Icon(Icons.backspace),
+              child: Icon(Icons.remove),
             ),
             FloatingActionButton(
               onPressed: () {
-                setState(() {
-                  cont = cont + 1;
-                });
+                reset();
+              },
+              child: Icon(Icons.autorenew),
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                add();
               },
               child: Icon(Icons.add),
             ),
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
